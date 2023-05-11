1d3d2fdffa45357e4e04ce6b4403d0d4ce2e7caa
gamesMHG <Games@mhgtechnologies.com>
2023-05-11 06:19:27 +0530

    game1 updated

--- Builds/altdrx/main.js
+++ Builds/altdrx/main.js
@@ -9,15 +9,15 @@ window.boot = function () {
     function setLoadingDisplay () {
         // Loading splash scene
         var splash = document.getElementById('splash');
-        var progressBar = splash.querySelector('.progress-bar span');
+        /*var progressBar = splash.querySelector('.progress-bar span');
         onProgress = function (finish, total) {
             var percent = 100 * finish / total;
             if (progressBar) {
                 progressBar.style.width = percent.toFixed(2) + '%';
             }
-        };
+        };*/
         splash.style.display = 'block';
-        progressBar.style.width = '0%';
+        //progressBar.style.width = '0%';
 
         cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
             splash.style.display = 'none';
